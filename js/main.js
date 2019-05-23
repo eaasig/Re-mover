d18e5e4b5fc96fadcda14237f468f39f3ef8b827
Irina <eaasig@students.eaaa.dk>
2019-05-23 13:06:27 +0200

    Added back+page title to all pages(if not needed use "hidden") + working nav on all pagess

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+
+    
 function openOverlay() {
   document.getElementById("overlayNav").style.width = "500px";
 }
 
 function closeOverlay() {
   document.getElementById("overlayNav").style.width = "0%";
+}
+
+function goBack() {
+  window.history.back();
 }
